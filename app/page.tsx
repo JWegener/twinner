96d094e8705ba84507dd5ef21c924959b5cf4486
Jonathan Wegener <github@jwegener.com>
2023-09-07 00:17:33 -0700

    copy tweak

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ export default function Chat() {
         <input
           className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
-          placeholder="Ask Rachel something...ex: how'd you sleep? Did you workout?"
+          placeholder="Ask me...ex: how'd you sleep? Did you workout?"
           onChange={handleInputChange}
           autoFocus
         />
